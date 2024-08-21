1317af7f49881e99c41ceb27eb566fe8e740d951
blag <blag@users.noreply.github.com>
2024-08-20 22:05:51 -0700

    Use request.headers instead of request.META

--- brake/tests/custom_backend.py
+++ brake/tests/custom_backend.py
@@ -2,7 +2,7 @@ from brake.backends import cachebe
 
 class MyBrake(cachebe.CacheBackend):
     def get_ip(self, request):
-        return request.META.get(
-            'HTTP_TRUE_CLIENT_IP',
+        return request.headers.get(
+            'true-client-ip',
             request.META.get('REMOTE_ADDR')
         )
